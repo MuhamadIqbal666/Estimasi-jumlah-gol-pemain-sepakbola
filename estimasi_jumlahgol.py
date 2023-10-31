8539c5a348c4f0a7eaac1f374d2120e987ced813
MuhamadIqbal666 <149354594+MuhamadIqbal666@users.noreply.github.com>
2023-10-31 13:04:33 +0700

    Update estimasi_jumlahgol.py

--- estimasi_jumlahgol.py
+++ estimasi_jumlahgol.py
@@ -15,11 +15,9 @@ Player_Names = st.text_input('Input Nama Pemain')
 Country = st.text_input('Input Negara Kompetisi Liga')
 League = st.text_input('Input Kompetesi Liga')
 Club = st.text_input('Input Club Pemain')
-Matches_Played = st.number_input('Jumlah Pertandingan yang telah dimainkan', 0)
-Mins = st.number_input('Jumlah menit bermain', 0)
-xG = st.number_input('Jumlah peluang yang menjadi gol', 0)
-xG_Per_Avg_Match = st.number_input('Jumlah peluang yang menjadi gol per rata-rata pertandingan', 0)
-Shots = st.number_input('Jumlah Tembakan', 0)
+Matches_Played = st.number_input('Jumlah Pertandingan yang telah dimainkan (max : 38)', 0)
+Mins = st.number_input('Jumlah menit bermain (max : 4177)', 0)
+Shots = st.number_input('Jumlah Tembakan (max:208)', 0)
 OnTarget = st.number_input('Jumlah Tembakan yang tepat sasaran', 0)
 Shots_Per_Avg_Match = st.number_input('Jumlah Tembakan per rata-rata pertandingan', 0)
 On_Target_Per_Avg_Match = st.number_input('Jumlah Tembakan yang tepat sasaran per rata-rata pertandingan', 0)
@@ -29,7 +27,7 @@ predict = ''
 
 if st.button('Estimasi jumlah gol'):
     try:
-        n_features = [[Mins, Matches_Played, xG, xG_Per_Avg_Match, Shots, OnTarget, Shots_Per_Avg_Match, On_Target_Per_Avg_Match, int(Year)]]
+        n_features = [[Mins, Matches_Played, Shots, OnTarget, Shots_Per_Avg_Match, On_Target_Per_Avg_Match, int(Year)]]
         n_features = np.array(n_features)
         predict = model.predict(n_features)
         st.write('Estimasi jumlah gol yang dihasilkan: ', int(predict[0]), 'Gol')
